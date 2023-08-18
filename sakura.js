bda2843c9575843bff8fec1a980f579acb7920e5
WNBC2023 <141196577+WNBC2023@users.noreply.github.com>
2023-08-18 13:38:15 +0900

    Add files via upload

---
+++ sakura.js
@@ -0,0 +1,38 @@
+ function osita(){
+    youso=document.getElementById('text01');
+    t=youso.value;
+    hyouji='よろしくお願いします。'+t+'さん';
+
+    gamenyouso = document.
+    getElementById('gamen1')
+    gamenyouso.innerHTML = hyouji;
+ }
+
+ function komatta(){
+    youso=document.getElementById('text02');
+    t=youso.value;
+    hyouji2='大丈夫ですよ。きっとあなたなら頑張れます。'+t+'ならあなたがだけで考えないで他人に相談してみましょう！';
+    gamenyouso = document.
+    getElementById('gamen2')
+    gamenyouso.innerHTML = hyouji2;
+
+    gamenyouso = document.
+    getElementById('gamen3')
+    gamenyouso.innerHTML = hyouji3;
+ }
+ 
+
+ function tanosii(){
+    youso=document.getElementById('text03');
+    t=youso.value;
+    hyouji3='その調子で楽しみながらやりましょう！楽しみながら'+t+'を続けましょう！';
+    gamenyouso = document.
+    getElementById('gamen3')
+    gamenyouso.innerHTML = hyouji3;
+
+    gamenyouso = document.
+    getElementById('gamen3')
+    gamenyouso.innerHTML = hyouji3;
+ }
+
+ 
